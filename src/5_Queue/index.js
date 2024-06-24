5f47b53a4e6b48b2dcb9eff414e7fcb49a331b4f
AWEHA Nicolas <awenico98@gmail.com>
2024-06-24 11:57:36 +0100

    resolution of exercise 5 of the dsa challenge

--- src/5_Queue/index.js
+++ src/5_Queue/index.js
@@ -1,8 +1,46 @@
-// Do not use arrays!
+class QNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 export class Queue {
-  enqueue(value) {}
+  constructor() {
+    this.front = null;
+    this.rear = null;
+  }
 
-  dequeue() {}
+  enqueue(value) {
+    const temp = new QNode(value);
+    if (this.rear === null) {
+      this.front = this.rear = temp;
+      return;
+    }
 
-  peek() {}
+    this.rear.next = temp;
+    this.rear = temp;
+  }
+
+  dequeue() {
+    if (this.front === null) {
+      return null;
+    }
+
+    const temp = this.front;
+    this.front = this.front.next;
+
+    if (this.front === null) {
+      this.rear = null;
+    }
+
+    return temp.value;
+  }
+
+  peek() {
+    if (this.front === null) {
+      return null;
+    }
+    return this.front.value;
+  }
 }
